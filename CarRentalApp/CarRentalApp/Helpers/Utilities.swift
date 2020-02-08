686b47a4463b4f65ab28037781421a1cfdf6d124
Raymond Hong <hongr@uoguelph.ca>
2020-02-08 18:31:33 -0500

    clean up utilities class

--- CarRentalApp/CarRentalApp/Helpers/Utilities.swift
+++ CarRentalApp/CarRentalApp/Helpers/Utilities.swift
@@ -27,31 +27,7 @@ class Utilities {
         content.lineBreakMode = .byWordWrapping
         content.numberOfLines = 0
     }
-    
-    class func styleWelcomeLabel(_ label:UILabel) {
-        label.font = UIFont(name: "Helvetica Neue", size: 30)
-        label.textColor = UIColor.white
-    }
-    
-    class func styleTextField(_ textfield:UITextField) {
         
-        // Create the bottom line
-        let bottomLine = CALayer()
-        //let rgbcolor = #colorLiteral(red: 0.1789781749, green: 0.663659811, blue: 0.3821850121, alpha: 1)
-        
-        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height -
-            2, width: textfield.frame.width, height: 2)
-        
-        bottomLine.backgroundColor = UIColor.black.cgColor
-        
-        //Remove border on text field
-        textfield.borderStyle = .none
-        
-        //Add the line to the text field
-        textfield.layer.addSublayer(bottomLine)
-    
-    }
-    
     class func styleFilledButton(_ button:UIButton) {
         
         //Filled rounded corner style
@@ -59,16 +35,7 @@ class Utilities {
             button.layer.cornerRadius = 6.0
             button.tintColor = UIColor.white
     }
-    
-    class func styleHollowButton(_ button:UIButton) {
         
-        //Hollow rounded corner style
-        button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.cornerRadius = 25.0
-        button.tintColor = UIColor.black
-    }
-    
     class func styleRentedLabel(_ label: UILabel) {
         
         label.backgroundColor = UIColor(
@@ -85,20 +52,6 @@ class Utilities {
         cell.layer.borderWidth = 0.2
         cell.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         cell.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-    }
-    
-    class func isPasswordValid(_ password: String) -> Bool {
-        
-        //Ensures password contains:
-        //Ensures password has a minimum of 8 characters
-        //Ensures password has at least one lowercase
-        //Ensures passward has at least one uppercase
-        //Ensures password has at least one digit
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@",
-                "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\\W]{8,}$")
-        print(passwordTest.evaluate(with: password))
-        return passwordTest.evaluate(with: password)
-        
     }
 }
 
